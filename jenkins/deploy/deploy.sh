ad0ac962e8aefbda983b6732625f1528763020d6
ompayat <ompayat@gmail.com>
2023-11-03 19:35:32 -0400

    path update

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo maven-project > /home/eqisthos/Desktop/jenkins-data/tmp/.auth
-echo $BUILD_TAG >> /home/eqisthos/Desktop/jenkins-data/tmp/.auth
-echo $PASS >> /home/eqisthos/Desktop/jenkins-data/tmp/.auth
+echo maven-project > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+echo $PASS >> /tmp/.auth
 
-scp -i /home/eqisthos/Desktop/jenkins-data/prod /home/eqisthos/Desktop/jenkins-data/tmp/.auth ubuntu@3.90.241.93:/tmp/.auth
-scp -i /home/eqisthos/Desktop/jenkins-data/prod /home/eqisthos/Desktop/jenkins-data/pipeline/jenkins/deploy/publish ubuntu@3.90.241.93:/tmp/publish
-ssh -i /home/eqisthos/Desktop/jenkins-data/prod ubuntu@3.90.241.93 "/tmp/publish"
+scp -i /opt/prod /tmp/.auth ubuntu@3.90.241.93:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish ubuntu@3.90.241.93:/tmp/publish
+ssh -i /opt/prod ubuntu@3.90.241.93 "/tmp/publish"
