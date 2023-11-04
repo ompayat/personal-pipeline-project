7f3bea2715d5264cea4cf4301ab401dcdb2965ec
ompayat <ompayat@gmail.com>
2023-11-03 21:31:46 -0400

    mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "############################"
 echo "####### Building JAR #######"
 echo "############################"
 
-WORKSPACE=/home/eqisthos/Desktop/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /home/eqisthos/Desktop/jenkins-data/jenkins_home/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /var/jenkins_home/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
